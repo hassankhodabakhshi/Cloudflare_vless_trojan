e09cfece265942927753ffa827105f6e655dafb2
hassan khodabakhshi <80225351+hassankhodabakhshi@users.noreply.github.com>
2025-07-02 21:59:16 +0330

    Update vless无需proxyip的nat64套壳版 (推荐使用).js

--- Vless_workers_pages/vless无需proxyip的nat64套壳版 (推荐使用).js
+++ Vless_workers_pages/vless无需proxyip的nat64套壳版 (推荐使用).js
@@ -1,7 +1,7 @@
 //nat64自动填充proxyip，无需且不支持proxyip设置
 import { connect } from "cloudflare:sockets";
 const WS_READY_STATE_OPEN = 1;
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "5fc4b09d-7728-4a1b-8033-083c58b47e3e";
 const cn_hostnames = [''];
 let CDNIP = '\u0077\u0077\u0077\u002e\u0076\u0069\u0073\u0061\u002e\u0063\u006f\u006d\u002e\u0073\u0067'
 // http_ip
